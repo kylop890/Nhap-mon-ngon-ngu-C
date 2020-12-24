1b22f5f178f3c4b25735e6d8823db5e6ca8b464b
kylop890 <74344042+kylop890@users.noreply.github.com>
2020-12-24 00:30:08 -0800

    Update Ghichu.c

--- Bài tập 3/Ghichu.c
+++ Bài tập 3/Ghichu.c
@@ -8,3 +8,21 @@ int main(void) {
  	
   return 0;
   }
+
+
+#include <stdio.h>
+
+int main(void) {
+ int tong = 0;
+ for(int dem = 1; dem <= 10; dem++){
+ 	tong = tong + dem;
+	 }
+	 printf("tong la: %d",tong);
+ tong = tong + 1; 
+ tong = tong + 2;
+ tong = tong + 3;
+ tong = tong + 4;
+
+ 	
+  return 0;
+  }
