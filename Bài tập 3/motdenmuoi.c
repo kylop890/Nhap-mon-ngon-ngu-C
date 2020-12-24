5191d3fb93eb84fb1d70444be61778b94aea09d2
kylop890 <74344042+kylop890@users.noreply.github.com>
2020-12-23 23:12:28 -0800

    Create motdenmuoi.c

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  for(int n = 1; n< 11; n++){
+    printf("%d/n");
+    }
+  int count;
+  for(count = 1 ; count < 11; count++){
+    printf("\n 1 2 3 4 5 6 7 8 9 10  ");  
+  }
+  
+  return 0;
+}
